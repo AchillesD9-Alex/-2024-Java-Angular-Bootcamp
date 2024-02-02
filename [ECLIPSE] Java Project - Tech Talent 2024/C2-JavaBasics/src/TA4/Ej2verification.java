6dd883ff8eb70b2df9fb9feeaa81892bbaebd119
AchillesD9-Alex <28.alex.roman@gmail.com>
2024-02-02 14:00:52 +0100

    Update Ej2 verificación

--- [ECLIPSE] Java Project - Tech Talent 2024/C2-JavaBasics/src/TA4/Ej2verification.java
+++ [ECLIPSE] Java Project - Tech Talent 2024/C2-JavaBasics/src/TA4/Ej2verification.java
@@ -3,9 +3,17 @@ package TA4;
 public class Ej2verification {
 
 	public static void main(String[] args) {
-		/* Escribe un programa Java que realice lo siguiente: declarar una variable N de tipo int,
-una variable A de tipo double y una variable C de tipo char y asigna a cada una un valor*/
-	
+		
+	//VERIFICACIÓN. CREO Ej2 para verificar la condición del ejercicio.
+		/*Si por ejemplo le hemos dado a N el valor 5, a A el valor 4.56 y a C el valor ‘a’, se debe
+mostrar por pantalla:
+• Variable N = 5
+• Variable A = 4.56
+• Variable C = a
+• 5 + 4.56 = 9.559999999999999
+• 4.56 - 5 = -0.4400000000000004
+• Valor numérico del carácter a = 97*/
+		
 		int n=5;
 		double a=4.56;
 		char c='a'; 
