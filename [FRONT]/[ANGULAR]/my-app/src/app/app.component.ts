344002adbc1e50aa4b228a4ce2ec020744c53cd3
Alexandre Román <158473735+AchillesD9-Alex@users.noreply.github.com>
2024-06-27 11:15:02 +0200

    angular images

--- [FRONT]/[ANGULAR]/my-app/src/app/app.component.ts
+++ [FRONT]/[ANGULAR]/my-app/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { AboutComponent } from './pages/about/about.component';
+import { TermsComponent } from './pages/terms/terms.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, AboutComponent, TermsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Mi primera app';
 }
